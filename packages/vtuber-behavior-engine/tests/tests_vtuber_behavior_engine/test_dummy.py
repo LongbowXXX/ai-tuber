a83d002357a58e83a035bd00bf7089b9c1cbd7db
LongbowXXX <50514445+LongbowXXX@users.noreply.github.com>
2025-05-04 15:58:34 +0900

    Add dummy test .

---
+++ packages/vtuber-behavior-engine/tests/tests_vtuber_behavior_engine/test_dummy.py
@@ -0,0 +1,9 @@
+#  Copyright (c) 2025 LongbowXXX
+#
+#  This software is released under the MIT License.
+#  http://opensource.org/licenses/mit-license.php
+
+
+def test_dummy() -> None:
+    """A dummy test to ensure the test suite is running."""
+    assert True
